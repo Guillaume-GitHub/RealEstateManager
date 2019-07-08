fd8bf9c036874ae0aac9e33ff441dbdc4be658b5
GuiGui <bague.guillaume@live.fr>
2019-07-08 15:47:52 +0200

    add color style and configure nav drawer

--- app/src/main/java/com/openclassrooms/realestatemanager/controller/MainActivity.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/controller/MainActivity.kt
@@ -2,7 +2,10 @@ package com.openclassrooms.realestatemanager.controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
 import com.openclassrooms.realestatemanager.R
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,10 +13,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         this.displayListFragment()
+        this.configToolbar()
+        this.configureDrawerLayout()
     }
 
     private fun displayListFragment(){
         val itemFragment = HomeFragment()
         supportFragmentManager.beginTransaction().add(R.id.activity_main_framelayout_list,itemFragment).commit()
+    }
+
+    private fun configToolbar(){
+        // Set the toolbar as support action bar
+        setSupportActionBar(activity_main_toolbar)
+        // get the support action bar
+        val actionBar = supportActionBar
+    }
+
+
+    // 2 - Configure Drawer Layout
+    private fun configureDrawerLayout() {
+        var drawerLayout = activity_main_drawer_layout
+        var toggle = ActionBarDrawerToggle(this, drawerLayout, activity_main_toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawerLayout.addDrawerListener(toggle)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.elevation = 5.0f
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_twotone_menu_white_24px)
     }
 }
